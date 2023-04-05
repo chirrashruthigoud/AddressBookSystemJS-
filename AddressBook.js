3c604dac362f6d4a00f080726fac4cab0cff5e0e
chirrashruthigoud <chirrashruthigoud4@gmail.com>
2023-04-05 12:39:32 +0530

    Address Booking Problem

--- AddressBook.js
+++ AddressBook.js
@@ -1,14 +1,19 @@
-const addressBook = [
-    { name: 'Alice', phone: '123-456-7890' },
-    { name: 'Bob', phone: '555-555-5555' },
-    { name: 'Charlie', phone: '987-654-3210' },
-    { name: 'David', phone: '555-123-4567' },
-    { name: 'Eve', phone: '555-123-4567' }
-  ];
-  
-  const numOfContacts = addressBook.reduce((count, contact) => {
-    return count + 1;
-  }, 0);
-  
-  console.log(numOfContacts); // Output: 5
-  
+let addressBook = [];
+
+function addPerson(name, email, phone) {
+  // check if person already exists in address book
+  const isDuplicate = addressBook.filter(person => person.name === name).length > 0;
+
+  // add person to address book if not a duplicate
+  if (!isDuplicate) {
+    addressBook.push({ name, email, phone });
+    console.log(`Added ${name} to address book.`);
+  } else {
+    console.log(`${name} already exists in address book.`);
+  }
+}
+
+// example usage
+addPerson('John Smith', 'john@example.com', '555-1234');
+addPerson('Jane Doe', 'jane@example.com', '555-5678');
+addPerson('John Smith', 'john.smith@example.com', '555-4321'); // this will be a duplicate entry
